1a8ec3dfaf35bea6eb4a874d05bbfdd80403f1d8
Liam Roche <roche.liamp@gmail.com>
2022-03-17 10:35:07 -0400

    added try catch blocks to all db query trip routes

--- controllers/tripController.js
+++ controllers/tripController.js
@@ -8,33 +8,38 @@ const Trip = require('../models/trip');
 //TRENDING FUNCTION - CALLED IN TRIP INDEX ROUTE TO POPULATE PAGE WITH TRENDING TRIPS
 //3 TRENDING MAX NUMER
 async function findTrendingTrips(){
-    const trendingTripArr = []
-    const trips = await Trip.find()
-    let highestHitTrip = trips[0]
-    let secondHighestTrip = trips[0]
-    let thirdHighest = trips[0]
-    for(let i = 0; i < trips.length; i++){
-        let trip = trips[i]
-        if(trip.hitcount > highestHitTrip.hitcount){
-            highestHitTrip = trip
+    try{
+        const trendingTripArr = []
+        const trips = await Trip.find()
+        let highestHitTrip = trips[0]
+        let secondHighestTrip = trips[0]
+        let thirdHighest = trips[0]
+        for(let i = 0; i < trips.length; i++){
+            let trip = trips[i]
+            if(trip.hitcount > highestHitTrip.hitcount){
+                highestHitTrip = trip
+            }
         }
-    }
-    trendingTripArr.push(highestHitTrip)
-    for(let i = 0; i < trips.length; i++){
-        let trip = trips[i]
-        if(trip.hitcount > secondHighestTrip.hitcount && trip._id !== highestHitTrip._id){
-            secondHighestTrip = trip
+        trendingTripArr.push(highestHitTrip)
+        for(let i = 0; i < trips.length; i++){
+            let trip = trips[i]
+            if(trip.hitcount > secondHighestTrip.hitcount && trip._id !== highestHitTrip._id){
+                secondHighestTrip = trip
+            }
         }
-    }
-    trendingTripArr.push(secondHighestTrip)
-    for(let i = 0; i < trips.length; i++){
-        let trip = trips[i]
-        if(trip.hitcount > thirdHighest.hitcount && trip._id !== highestHitTrip._id && trip._id !== secondHighestTrip._id){
-            thirdHighest = trip
+        trendingTripArr.push(secondHighestTrip)
+        for(let i = 0; i < trips.length; i++){
+            let trip = trips[i]
+            if(trip.hitcount > thirdHighest.hitcount && trip._id !== highestHitTrip._id && trip._id !== secondHighestTrip._id){
+                thirdHighest = trip
+            }
         }
+        trendingTripArr.push(thirdHighest)
+        return(trendingTripArr)
+    }catch(err){
+        console.log(err)
+        res.sendStatus(500)
     }
-    trendingTripArr.push(thirdHighest)
-    return(trendingTripArr)
 }
 
 
@@ -42,24 +47,34 @@ async function findTrendingTrips(){
 // TRIP INDEX PAGE [1/7]
 //     -World map showing user destinations
 router.get('/', async (req, res) => {
-    const trips = await Trip.find();
-    const trendingTrips = await findTrendingTrips();
-    const userId = (req.session.userId)
-    res.render('../views/trips/index.ejs', {
-        isLoggedIn: req.session.isLoggedIn,
-        trips: trips,
-        userId: userId,
-        trendingTrips: trendingTrips
-    })
+    try{
+        const trips = await Trip.find();
+        const trendingTrips = await findTrendingTrips();
+        const userId = (req.session.userId)
+        res.render('../views/trips/index.ejs', {
+            isLoggedIn: req.session.isLoggedIn,
+            trips: trips,
+            userId: userId,
+            trendingTrips: trendingTrips
+        })
+    }catch(err){
+        console.log(err)
+        res.sendStatus(500)
+    }
 })
 
 
 //TEMP GOOGLE MAP ROUTE
 router.get('/googlemaps', async (req, res) => {
-    const trips = await Trip.find();
-    const userId = (req.session.userId)
-    res.render('../views/trips/tempGoogle.ejs', {
-    })
+    try{
+        const trips = await Trip.find();
+        const userId = (req.session.userId)
+        res.render('../views/trips/tempGoogle.ejs', {
+        })
+    }catch(err){
+        console.log(err)
+        res.sendStatus(500)
+    }
 })
 
 
@@ -67,12 +82,13 @@ router.get('/googlemaps', async (req, res) => {
 //     -Renders form to create trip
 //     -Re-directs back to user show page
 router.get('/new/:userID', async (req, res) => {
-    try {
+    try{
         const user = await User.findById(req.params.userID)
         res.render('../views/trips/new.ejs', {
             user: user
         })
-    } catch (err) {
+    }catch (err) {
+        console.log(err)
         res.send("something with router . get")
     }
 })
@@ -82,7 +98,7 @@ router.get('/new/:userID', async (req, res) => {
 //     -Create the actual trip  
 router.post('/:id/:UserID', async (req, res) => {
     //TRY BLOCK FOR DB QUERY
-    try {
+    try{
         // sets the user of the trip ot be the current user's id
         // res.locals.userId is from your app.js when you created the middleware -- you can use it anywhere (it's global)
         req.body.user = res.locals.userId
@@ -97,9 +113,9 @@ router.post('/:id/:UserID', async (req, res) => {
         // console.log(user.trips.length)
         res.redirect(`/users/${user._id}`)
         //DB OOPSIES
-    } catch (err) {
-        res.send("something with router . post")
+    }catch (err) {
         console.log(err)
+        res.send("something with router . post")
     }
 })
 
@@ -108,30 +124,33 @@ router.post('/:id/:UserID', async (req, res) => {
 //     -Lists price of current trip
 //     -Possible addition of more information
 router.get('/:id', async (req, res) => {
-    const trip = await Trip.findById(req.params.id).populate('user') // now you can well actually you would still have the user id without .populate but you didn't have it before because you weren't adding it when you posted a trip
-    //THIS WILL INCREMENT THE HITCOUNT STORED IN THE TRIP MODEL WHETHER USER WHO CLICKS IS LOGGED IN OR NOT
-    trip.hitcount++
-    trip.save()
-    console.log(trip.hitcount)
-    if (req.session.isLoggedIn) {
-
-        console.log("logged in")
-
-        let tripOwner = trip.user._id + ""
-        let currUser = req.session.userId + ""
-        let tripOwnerName = trip.user.username
-        // console.log(`key :${user.keys}`)
-        // if (res.locals.userId == trip.user ) --> not sure if user would just give you the id, that's something you would want to console log so you can see that actual obj in your terminal and if it doesn't if you the id then you could use .populate
+    try{
+        const trip = await Trip.findById(req.params.id).populate('user') // now you can well actually you would still have the user id without .populate but you didn't have it before because you weren't adding it when you posted a trip
+        //THIS WILL INCREMENT THE HITCOUNT STORED IN THE TRIP MODEL WHETHER USER WHO CLICKS IS LOGGED IN OR NOT
+        trip.hitcount++
+        trip.save()
+        console.log(trip.hitcount)
+        if (req.session.isLoggedIn) {
+            console.log("logged in")
+            let tripOwner = trip.user._id + ""
+            let currUser = req.session.userId + ""
+            let tripOwnerName = trip.user.username
+            // console.log(`key :${user.keys}`)
+            // if (res.locals.userId == trip.user ) --> not sure if user would just give you the id, that's something you would want to console log so you can see that actual obj in your terminal and if it doesn't if you the id then you could use .populate
             res.render("../views/trips/show.ejs", {
                 trip: trip,
                 tripOwner: tripOwner,
                 currUser :currUser,
                 tripOwnerName :tripOwnerName
             })
-    } 
-    else {
-        res.redirect('/users')
-        console.log("not logged in")
+        }    
+        else{
+            res.redirect('/users')
+            console.log("not logged in")
+        }
+    }catch(err){
+        console.log(err)
+        res.sendStatus(500)
     }
 })
 
@@ -139,14 +158,14 @@ router.get('/:id', async (req, res) => {
 // TRIP SHOW FORM TO EDIT PAGE [5/7]
 //     -Show form to edit trip
 router.get('/:id/edit', async (req, res) => {
-    try {
+    try{
         const trip = await Trip.findById(req.params.id)
         res.render('../views/trips/edit.ejs', {
             trip: trip
         })
-    } catch (err) {
+    }catch(err){
+        console.log(err)
         res.sendStatus(500)
-
     }
 })
 
@@ -154,7 +173,7 @@ router.get('/:id/edit', async (req, res) => {
 // TRIP EDIT ROUTE [6/7]
 //     -Edits the trip, dates, layovers
 router.put('/:id', async (req, res) => {
-    try {   
+    try{   
         //UPDATES TRIP IN DB
         await Trip.findByIdAndUpdate(req.params.id, req.body)
         //FINDS UPDATED TRIP IN DB
@@ -174,7 +193,7 @@ router.put('/:id', async (req, res) => {
         }  
         //SEND THEM BACK TO THE SHOW PAGE (OR MY TRIPS PAGE???)
         res.redirect(`/trips/${req.params.id}`)
-    } catch (err) {
+    }catch(err){
         console.log(err)
         res.sendStatus(500)
     }
@@ -207,6 +226,7 @@ router.delete('/:id', async (req, res) => {
         res.redirect(`/users/${user._id}`)
         //DB FUCK-UPS
     } catch (err) {
+        console.log(err)
         res.sendStatus(500)
     }
 })
